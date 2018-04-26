a9060d7ba975f8d95d55f3623b4a18c2477a83e4
Bobby Martin and Kristen Chellis <kristen@>
2018-04-26 10:30:32 -0700

    refactor so that we only make 3 api calls not one every question

--- src/api.js
+++ src/api.js
@@ -1,15 +1,18 @@
 class API {
-  constructor(category, difficulty) {
+  constructor(category) {
     this.category = category;
-    this.difficulty = difficulty;
+    this.count = [];
+    this.resultsHard;
+    this.resultsMedium;
+    this.resultsEasy;
     this.question;
     this.correct;
     this.options;
   }
-  makeCall() {
-    let promise = new Promise ((resolve, reject) => {
+  callCount() {
+    return new Promise ((resolve, reject) => {
       let request = new XMLHttpRequest();
-      let url = `https://opentdb.com/api.php?amount=1&category=${this.category}&difficulty=${this.difficulty}&type=multiple`;
+      let url = `https://opentdb.com/api_count.php?category=${this.category}`;
       request.onload = function() {
         if (this.status === 200) {
           resolve(request.response);
@@ -20,15 +23,88 @@ class API {
       request.open("GET", url, true);
       request.send();
     });
+  }
+  callQuestions(difficulty) {
+    let promise = new Promise ((resolve, reject) => {
+      let request = new XMLHttpRequest();
+      let count;
+      if (difficulty === "hard") {
+        count = this.count[2];
+      }else if (difficulty === "medium") {
+        count = this.count[1];
+      }else if (difficulty === "easy") {
+        count = this.count[0];
+      }
 
+      let url = `https://opentdb.com/api.php?amount=${count-1}&category=${this.category}&difficulty=${difficulty}&type=multiple`;
+      console.log(url);
+      request.onload = function() {
+        if (this.status === 200) {
+          resolve(request.response);
+        }else {
+          reject(Error(request.statusText));
+        }
+      };
+      request.open("GET", url, true);
+      request.send();
+    });
     promise.then((response) => {
-      let body = JSON.parse(response);
-      this.question = body.results[0].question;
-      this.correct = body.results[0].correct_answer;
-      this.options = body.results[0].incorrect_answers;
-      this.options.push(this.correct);
+      if (difficulty === "hard") {
+        this.resultsHard = JSON.parse(response);
+      }else if (difficulty === "medium") {
+        this.resultsMedium = JSON.parse(response);
+      }else if (difficulty === "easy") {
+        this.resultsEasy = JSON.parse(response);
+      }
     });
   }
+
+  callback(difficulty) {
+    this.callCount()
+      .then((response) => {
+        let body = JSON.parse(response);
+        let easy =body.category_question_count.total_easy_question_count
+        let medium = body.category_question_count.total_medium_question_count
+        let hard =body.category_question_count.total_hard_question_count
+        if(easy <= 50) {
+          this.count.push(easy);
+        }else {
+          this.count.push(50);
+        }
+        if(medium <= 50) {
+          this.count.push(medium);
+        }else {
+          this.count.push(50);
+        }
+        if(hard <= 50) {
+          this.count.push(hard-7);
+        }else {
+          this.count.push(50);
+        }
+        return this.callQuestions(difficulty);
+      });
+
+  }
+
+  getQuestion(difficulty) {
+    let array;
+    if (difficulty === "hard") {
+      array = this.resultsHard.results;
+    }else if (difficulty === "medium") {
+      array = this.resultsMedium.results;
+    }else if (difficulty === "easy") {
+      array = this.resultsEasy.results;
+    }
+    console.log(array);
+    let max = array.length - 1;
+    let index = Math.floor(Math.random() * (max + 1));
+    this.question = array[index].question;
+    this.correct = array[index].correct_answer;
+    this.options = array[index].incorrect_answers;
+    this.options.push(this.correct);
+    array.splice(index, 1);
+  }
+
   checkAnswer(answer) {
     if (answer === this.correct) {
       return true;
--- src/main.js
+++ src/main.js
@@ -21,6 +21,15 @@ $(function() {
     let name = $("#name").val();
     category = parseInt($("#category").val());
     user = new User(name);
+    api = new API(category);
+    api.callback("easy");
+    setTimeout(() => {
+      api.callQuestions("medium");
+    }, 5000);
+    setTimeout(() => {
+      api.callQuestions("hard");
+      console.log(api);
+    }, 10000);
     $(".user").text(name);
   });
 
@@ -30,19 +39,17 @@ $(function() {
     $(".incorrect").hide();
     $("#next").text("Next Question");
     user.checkLevel();
-    api = new API(category, user.difficulty);
-    api.makeCall();
-    setTimeout(() => {
-      api.shuffleArray();
-      $("#question").text("");
-      $("#question").append(api.question);
-      for (var i = 0; i < 4; i++) {
-        let array = ["#optionA", "#optionB","#optionC","#optionD"];
-        $(array[i]).text("");
-        $(array[i]).append(api.options[i]);
-      }
-      $(".question-container").show().addClass("box-shadow");
-    }, 1500);
+    api.getQuestion(user.difficulty);
+    api.shuffleArray();
+    $("#question").text("");
+    $("#question").append(api.question);
+    for (var i = 0; i < 4; i++) {
+      let array = ["#optionA", "#optionB","#optionC","#optionD"];
+      $(array[i]).text("");
+      $(array[i]).append(api.options[i]);
+    }
+    $(".question-container").show().addClass("box-shadow");
+
   });
 
   $(".option").click(function() {
